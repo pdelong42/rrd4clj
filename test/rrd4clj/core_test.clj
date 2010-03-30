c217287a74ae4677afd7daf9bc89e7e9de0e535c
maoe <maoe.maoe@gmail.com>
2010-03-30 19:59:12 +0900

    add some tests

--- test/rrd4clj/core_test.clj
+++ test/rrd4clj/core_test.clj
@@ -1,6 +1,21 @@
 (ns rrd4clj.core-test
   (:use [rrd4clj.core] :reload-all)
-  (:use [clojure.test]))
+  (:use rrd4clj.imports)
+  (:use [clojure.test]
+        [clojure.contrib.import-static])
+  (:import [org.rrd4j.core DsDef ArcDef])
 
-(deftest replace-me ;; FIXME: write
-  (is false))
+(import-static org.rrd4j.ConsolFun AVERAGE FIRST LAST MAX MIN TOTAL)
+(import-static org.rrd4j.DsType ABSOLUTE COUNTER DERIVE GAUGE)
+
+(deftest test-data-source
+  (let [ds (data-source "name" COUNTER 300 0 Double/NaN)]
+    (are [expr answer] (= expr answer)
+      (class ds) DsDef)))
+
+(deftest test-rr-archive
+  (let [rra (rr-archive AVERAGE 0.5 1 600)]
+    (are [expr answer] (= expr answer)
+      (class raa) ArcDef)))
+
+  
---
+++ test/rrd4clj/io_test.clj
@@ -0,0 +1,7 @@
+(ns rrd4clj.io-test
+  (:use [rrd4clj.io] :reload-all)
+  (:use [rrd4clj.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is true))
