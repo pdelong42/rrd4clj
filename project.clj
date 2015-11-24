c6a32f99e84993cbf8e4bc467fed383d05f6ef2a
Paul DeLong <Paul.DeLong@gmail.com>
2015-11-24 12:56:29 -0500

    stop getting rrd4j the hard way
    After hitting some compile-time errors caused by new features in rrd4j,
    I realized that it wasn't getting the dependencies it needed.  I briefly
    started looking into getting lein-javac to run Maven, when it hit me
    that I was reinventing the wheel.  So I simply pulled-in rrd4j as a
    project dependency from clojars, rather than using the submodule.
    
    If this doesn't break anything else in the end, I'm ripping-out the
    submodule.  Maoe was only including it as a submodule because of his
    patch.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
 ;      [org.clojure/clojure "1.2.0-master-SNAPSHOT"]
 ;      [org.clojars.maoe/funky "0.0.6"]
       [berkeleydb/je "3.2.76"]
+      [org.rrd4j/rrd4j "2.2.1"]
     ]
    :dev-dependencies [
       [leiningen/lein-swank "1.1.0"]
@@ -14,6 +15,6 @@
 ;      [lein-javac "0.0.2-SNAPSHOT"]
                       ]
 ;   :source-path "src/clj"
-   :java-source-paths ["src/java"]
+;   :java-source-paths ["src/java"]
    :main rrd4clj.examples
    :namespaces [rrd4clj.examples rrd4clj.core rrd4clj.io]  )
