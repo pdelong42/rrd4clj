4a243923788f21a36500ff0ba0ab79c25104314d
Paul DeLong <Paul.DeLong@gmail.com>
2015-12-09 22:25:49 -0500

    replaced a let with a lambda
    I replaced a level of let with a function call, and in the process made
    things a little neater (removing an unnecessary level of indentation).

--- src/rrd4clj/examples.clj
+++ src/rrd4clj/examples.clj
@@ -28,49 +28,50 @@
    [file]
    (format "%s%s%s" (demo-dir) File/separator file)  )
 
+(defn min-max-demo [start end rrd-path graph-path]
+   (let ; this line originally used io/with-rrd
+      [  rrdi
+         (RrdDb.
+            (rrd
+               rrd-path
+               {  :start-time (dec start)
+                  :step       300  }
+               (->DataSource "a" GAUGE 600 Double/NaN Double/NaN)
+               (->RoundRobinArchive AVERAGE 0.5  1 300)
+               (->RoundRobinArchive MIN     0.5 12 300)
+               (->RoundRobinArchive MAX     0.5 12 300)  )  )  ]
 
-(defn min-max-demo []
+      (apply io/update_rrd rrdi
+         (for
+            [t (range start end 300)]
+            (sample t (+ 50 (* 50 (Math/sin (/ t 3000.0)))))  )  )
+
+      (io/graph
+         (g/graph graph-path
+            {  :width 450
+               :height 250
+               :image-format "PNG"
+               :start-time start
+               :end-time (+ start 86400)
+               :title "rrd4clj's MINMAX demo"
+               :anti-aliasing false  }
+            (g/->DataSource "a" rrd-path "a" AVERAGE)
+            (g/->DataSource "b" rrd-path "a" MIN)
+            (g/->DataSource "c" rrd-path "a" MAX)
+            (g/->CDefSource "d" "a,-1,*")
+            (g/->Area "a" (Color/decode "0xb6e4") "real")
+            (g/->Line "b" (Color/decode "0x22e9") "min")
+            (g/->Line "c" (Color/decode "0xee22") "max")
+            (g/stack-of
+               (g/->Area "d" (Color/decode "0xb6e4") "inv")
+               (g/->Area "d" (Color/decode "0xfffe") "stack")
+               (g/->Area "d" (Color/decode "0xeffe") "stack2")  )  )  )  )  )
+
+(defn -main []
    (let
-      [  start      (getTime)
-         end        (+ start (* 300 300))
-         rrd-path   (demo-path "minmax.rrd")
-         graph-path (demo-path "minmax.png")  ]
-      (let
-         [  rrdi
-            (RrdDb.
-               (rrd rrd-path
-                  {  :start-time (dec start)
-                     :step       300  }
-                  (->DataSource "a" GAUGE 600 Double/NaN Double/NaN)
-                  (->RoundRobinArchive AVERAGE 0.5  1 300)
-                  (->RoundRobinArchive MIN     0.5 12 300)
-                  (->RoundRobinArchive MAX     0.5 12 300)  )  )  ]
-
-         ;; update
-         (apply io/update_rrd rrdi
-            (for [t (range start end 300)]
-               (sample t (+ 50 (* 50 (Math/sin (/ t 3000.0)))))  )  )
-
-         ;; graph
-         (io/graph
-            (g/graph graph-path
-               {  :width 450
-                  :height 250
-                  :image-format "PNG"
-                  :start-time start
-                  :end-time (+ start 86400)
-                  :title "rrd4clj's MINMAX demo"
-                  :anti-aliasing false  }
-               (g/->DataSource "a" rrd-path "a" AVERAGE)
-               (g/->DataSource "b" rrd-path "a" MIN)
-               (g/->DataSource "c" rrd-path "a" MAX)
-               (g/->CDefSource "d" "a,-1,*")
-               (g/->Area "a" (Color/decode "0xb6e4") "real")
-               (g/->Line "b" (Color/decode "0x22e9") "min")
-               (g/->Line "c" (Color/decode "0xee22") "max")
-               (g/stack-of
-                  (g/->Area "d" (Color/decode "0xb6e4") "inv")
-                  (g/->Area "d" (Color/decode "0xfffe") "stack")
-                  (g/->Area "d" (Color/decode "0xeffe") "stack2")  )  )  )  )  )  )
-
-(defn -main [] (min-max-demo))
+      [  now (getTime)  ]
+      (min-max-demo
+         now
+         (+ now (* 300 300))
+         (demo-path "minmax.rrd")
+         (demo-path "minmax.png")  )  )  )
